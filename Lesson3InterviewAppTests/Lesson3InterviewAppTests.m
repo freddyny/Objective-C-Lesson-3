e83bb3cd96f6c7736462a479d54462b6edefbde2
Frederik Nygaard <frederiknygaard@frederiks-air.attlocal.net>
2016-05-19 19:24:04 -0700

    Initial Commit

---
+++ Lesson3InterviewAppTests/Lesson3InterviewAppTests.m
@@ -0,0 +1,40 @@
+//
+//  Lesson3InterviewAppTests.m
+//  Lesson3InterviewAppTests
+//
+//  Created by Frederik Nygaard on 19.05.16.
+//  Copyright (c) 2016 Frederik Nygaard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Lesson3InterviewAppTests : XCTestCase
+
+@end
+
+@implementation Lesson3InterviewAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
